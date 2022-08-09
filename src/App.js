5c48b8379b3747887734fdabd19de5468a90c902
loteknowledG <loteknowledG@gmail.com>
2022-08-09 10:12:38 -0400

    submit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Chart } from "../components/Chart";
+import { Chart } from "./components/Chart";
 import "./styles.css";
 
 export default function App() {
